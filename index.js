672d0414b1430b0f971c74df06ae2724788092f3
redolent-history-c933 <nathoff22@gmail.com>
2021-02-25 03:08:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,18 +10,4 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-var uppercase = "HELLO!"
-uppercase.toUpperCase() === uppercase
-var lowercase = "hello!"
-lowercase.toLowerCase() === lowercase
-var mixedCase = "Hi there!"
-mixedCase.toLowerCase() === mixedCase
-mixedCase.toUpperCase() === mixedCase
 
-function sayHiToGrandma(string) {
-  if uppercase return "YES INDEED!"
-  if lowercase return "I can't hear you!"
-  if mixedCase return "I love you, too."
-  
-}
-function sayHiToGrandma(string) {
